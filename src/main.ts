a0976b003a30bb59fa9520108d44b60884d32fbd
victorLopz <castillolopez735@gmail.com>
2024-06-20 00:48:45 -0600

    chore: Update .gitignore to include package-lock.json and yarn.lock files, and .env file

--- src/main.ts
+++ src/main.ts
@@ -15,7 +15,7 @@ async function bootstrap() {
     .build();
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('docs', app, document);
-  app.useGlobalPipes( new ValidationPipe());
+  app.useGlobalPipes(new ValidationPipe());
   await app.listen(3000);
 }
 bootstrap();
